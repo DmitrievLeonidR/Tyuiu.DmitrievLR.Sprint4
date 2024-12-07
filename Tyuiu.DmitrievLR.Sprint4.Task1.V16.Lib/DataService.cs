7f9b9cf2a51ad43cdef928aed2f1b54b4ee3e0b6
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-07 16:46:16 +0500

    Task1

---
+++ Tyuiu.DmitrievLR.Sprint4.Task1.V16.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.DmitrievLR.Sprint4.Task1.V16.Lib
+{
+    public class DataService : ISprint4Task1V16
+    {
+        public int Calculate(int[] array)
+        {
+            int res = 1;
+            for (int i = 0; i < 13; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    res *= array[i];
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint4.Task1.V16/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.DmitrievLR.Sprint4.Task1.V16.Lib;
+namespace Tyuiu.DmitrievLR.Sprint4.Task1.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+           
+            Console.WriteLine("Введите 13 чисел в диапазоне от 3 до 8:");
+            int[] array = new int[13];
+            for (int i = 0; i < 13; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(array);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
