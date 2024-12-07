395c57ccdd10cac5ad48e052dfb013a05bc150c8
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-07 15:32:36 +0500

    Sprint4

---
+++ Tyuiu.DmitrievLR.Sprint4.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint4.Task0.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint4.Task0.V26/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.DmitrievLR.Sprint4.Task0.V26.Lib;
+namespace Tyuiu.DmitrievLR.Sprint4.Task0.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int[] array = new int[] { 9, 3, 7, 1, 5, 5, 3, 2, 1, 7 };
+            var str = string.Join(" ", array);
+            Console.WriteLine(str);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumOddArrEl(array);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
