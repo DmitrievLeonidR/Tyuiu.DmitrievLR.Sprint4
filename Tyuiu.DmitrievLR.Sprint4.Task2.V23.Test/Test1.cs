87e05d2e159573d2dd9d336d9db767c73c7a42ed
DmitrievLeonidR <Tyuiu.DmitrievLR2@outlook.com>
2024-12-07 17:00:35 +0500

    Task2

---
+++ Tyuiu.DmitrievLR.Sprint4.Task2.V23.Test/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.DmitrievLR.Sprint4.Task2.V23.Test;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ Tyuiu.DmitrievLR.Sprint4.Task2.V23/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.DmitrievLR.Sprint4.Task2.V23.Lib;
+namespace Tyuiu.DmitrievLR.Sprint4.Task2.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Дмитриев Леонид Романович | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] Rnumbers = new int[15];
+            Random random = new Random();
+            for (int i = 0; i < Rnumbers.Length; i++)
+            {
+                Rnumbers[i] = random.Next(3, 9);
+            }
+
+            var str = string.Join(" ", Rnumbers);
+            Console.WriteLine(str);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.Calculate(Rnumbers);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
